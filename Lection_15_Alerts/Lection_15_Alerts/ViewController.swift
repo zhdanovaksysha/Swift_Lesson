ee62d6d5b9abda7b08ca8948f863757364354118
Ксюша Жданова <zhdanovaksysha@gmail.com>
2019-07-08 20:41:04 +0300

    -- added project

---
+++ Lection_15_Alerts/Lection_15_Alerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lection_15_Alerts
+//
+//  Created by Ксения on 7/8/19.
+//  Copyright © 2019 Zhdanova Ksysha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
